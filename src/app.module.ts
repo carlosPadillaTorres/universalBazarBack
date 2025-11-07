d73c0498a7c8c22e2a8056ed60dbc91d48fab495
carlosPadillaTorres <carlos3.dp.t@gmail.com>
2025-11-07 16:33:28 -0600

    feat(sales): add sales module, DTOs, entity, controller and examples

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { SalesModule } from './sales/sales.module';
 import { MongoConnection } from './config/db.config';
 
 @Module({
-  imports: [ProductsModule, MongoConnection],
+  imports: [ProductsModule, SalesModule, MongoConnection],
   controllers: [AppController],
   providers: [AppService],
 })
